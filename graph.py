da9b0cdb9b172097b656c2f40625f07c5d467faa
Harshitavemali <vemaliharshita1103@gmail.com>
2024-07-14 21:21:46 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,1 +1,19 @@
+graph = {}
 
+
+n = int(input("Enter the number of nodes: "))
+
+
+for i in range(n):
+    ver = input("Enter the vertex: ")
+    neigh = input("Enter the neighboring nodes (comma-separated): ").split(',')
+    graph[ver] = neigh
+
+
+startnode = input("Enter the start node: ")
+
+
+if startnode in graph:
+    print(f"Neighbors of {startnode}: {graph[startnode]}")
+else:
+    print(f"{startnode} is not present in the graph")
